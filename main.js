4e9bb470076ebdba54545de8cda42175752ce513
Eduardo Bravo <yahir.eduardo.bravo@gmail.com>
2024-03-07 11:23:28 -0500

    feat(src): add a more atomic desig

--- main.js
+++ main.js
@@ -4,7 +4,11 @@ document.addEventListener("alpine:init", () => {
      * array ; []
      * Options to be shown
      */
-    options: [],
+    options: [
+      { value: "opt1", text: "Option 1" },
+      { value: "opt2", text: "Option 2" },
+      { value: "opt3", text: "Option 3" },
+    ],
     /**
      * array ; []
      * An array of the initial selected values
@@ -27,22 +31,14 @@ document.addEventListener("alpine:init", () => {
      */
     createOnBlur: false,
     /**
-     * RegExp|string|function; null
-     * Specifies a RegExp or a string containing a regular expression 
-     * that the current search filter must match to be allowed to be 
-     * created. May also be a predicate function that takes the filter 
-     * text and returns whether it is allowed.
-     */
-    createFilter: null,
-    /**
      * string; ','
      * Delimiter for multiple values to create
      */
     delimiter: ",",
-    /** 
+    /**
      * boolean; false
      * Highlight matches
-    */
+     */
     highlight: false,
     /**
      * boolean; true
@@ -59,36 +55,31 @@ document.addEventListener("alpine:init", () => {
      * Maximum number of options to show, null = unlimited
      */
     maxOptions: 50,
-    /** 
+    /**
      * number; null
      * Maximum number of items to allow to select; 1 = single, null = unlimited
-    */
+     */
     maxItems: null,
-    /** 
+    /**
      * boolean; null (true in multiselect mode, false in singleselect mode)
      * Hide selected values in the dropdown (true in multiselect mode, false in singleselect mode)
-    */
+     */
     hideSelected: null,
-    /** 
+    /**
      * boolean; undefined (false in multiselect mode, true in singleselect mode)
      * Close the dropdown after selecting an option
-    */
+     */
     closeAfterSelect: undefined,
     /**
      * boolean; false
      * Allow an empty option
      */
     allowEmptyOption: false,
-    /** 
+    /**
      * number; 300
      * Throttle time in milliseconds to wait before searching
-    */
+     */
     loadThrottle: 300,
-    /** 
-     * string; 'loading'
-     * Class to apply to the loading element
-    */
-    loadingClass: 'loading',
     /**
      * string; undefined
      * Placeholder string
@@ -99,29 +90,20 @@ document.addEventListener("alpine:init", () => {
      * Hide the placeholder when the control has not focus and one or more selected items
      */
     hidePlaceholder: null,
-    /**
-     * boolean|string; false
-     * If true, the load function will be called upon control initialization 
-     * (with an empty search). Alternatively it can be set to 'focus' to call 
-     * the load function when control receives focus.
-     */
-    preload: false,
-    /**
-     * string; null
-     * Parent element to append the dropdown
-     */
-    dropdownParent: null,
-    /**
-     * boolean; false
-     * Add precedence to the search; If true, the "Add..." 
-     * option is the default selection in the dropdown.
-     */
-    addPrecedence: false,
-    /** 
-     * boolean; false
-     * Select on tab
-    */
-    selectOnTab: false,
-
+    
+    init() {
+        if (this.hideSelected === undefined){
+            this.hideSelected =  maxItems != 1 ;
+        }
+        if (this.closeAfterSelect === undefined){
+            this.closeAfterSelect = maxItems == 1;
+        }
+        if (this.placeholder === undefined){
+            this.placeholder = "Select an option";
+        }
+        if (this.hidePlaceholder === null){
+            this.hidePlaceholder = maxItems == 1;
+        }
+    },
   }));
 });
